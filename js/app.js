07dbf17c0f5adbb3c4177da3fc8d5b51fbe0d83c
tdccw <tdccw.droid@gmail.com>
2016-02-02 16:32:51 +0800

    Update with questions

--- js/app.js
+++ js/app.js
@@ -13,11 +13,137 @@ app.config(function($stateProvider, $urlRouterProvider) {
 	  
 	$urlRouterProvider.otherwise('/');  
 });
- 
+
 app.controller('MainCtrl', function($scope, $state, $ionicPopup) {
 
-	$scope.answers = [null, null, null]; 			// Initialize model here
-	$scope.correctAns = ['A', 'B', 'C'];
+	$scope.questions = [
+		{
+			id: 'q1',
+			qn: 'What does I&E day stand for?',
+			opts: [
+				{
+					val: 'A',
+					ans: 'Innovation & Enterprise Day',
+					correctAns: true
+				},
+				{
+					val: 'B',
+					ans: 'Innovation & Entertainment Day',
+					correctAns: false
+				},
+				{
+					val: 'C',
+					ans: 'Innovation & Education Day',
+					correctAns: false
+				},
+				{
+					val: 'D',
+					ans: 'Innovation & Environment Day',
+					correctAns: false
+				}
+			]
+		},
+		{
+			id: 'q2',
+			qn: 'How many SEIT students went for overseas industrial attachment in Taiwan in 2015?',
+			opts: [
+				{
+					val: 'A',
+					ans: '7',
+					correctAns: false
+				},
+				{
+					val: 'B',
+					ans: '8',
+					correctAns: true
+				},
+				{
+					val: 'C',
+					ans: '9',
+					correctAns: false
+				},
+				{
+					val: 'D',
+					ans: '10',
+					correctAns: false
+				}
+			]
+		},
+		{
+			id: 'q3',
+			qn: 'What is the maximum amount that ITE will fund each student for Enterprise Fund project?',
+			opts: [
+				{
+					val: 'A',
+					ans: '$1000',
+					correctAns: false
+				},
+				{
+					val: 'B',
+					ans: '$2000',
+					correctAns: false
+				},
+				{
+					val: 'C',
+					ans: '$3000',
+					correctAns: true
+				},
+				{
+					val: 'D',
+					ans: '$4000',
+					correctAns: false
+				}
+			]
+		},
+		{
+			id: 'q4',
+			qn: 'Which of the following is NOT a basic ingredient that is required for beer brewing?',
+			opts: [
+				{
+					val: 'A',
+					ans: 'Grape Juice',
+					correctAns: true
+				},
+				{
+					val: 'B',
+					ans: 'Malted Barley',
+					correctAns: false
+				},
+				{
+					val: 'C',
+					ans: 'Water',
+					correctAns: false
+				},
+				{
+					val: 'D',
+					ans: 'Yeast',
+					correctAns: false
+				}
+			]
+		}
+	];
+	
+	$scope.optIndex = function (index) {
+		return String.fromCharCode('a'.charCodeAt() + index);	// returns a, b, c, ...
+	}
+
+	function resetAnswers() {
+		$scope.answers = [];
+		$scope.correctAns = [];
+		for (var i=0; i<$scope.questions.length; i++) {
+			$scope.answers.push(null);
+			
+			for (var j=0; j<$scope.questions[i].opts.length; j++) {
+				if ($scope.questions[i].opts[j].correctAns)
+					$scope.correctAns.push($scope.questions[i].opts[j].val);
+			}
+		}
+	}
+	resetAnswers();							// reset form
+	
+	console.log('Correct answers = ', $scope.correctAns);
+
+	$scope.minCorrectAns = $scope.questions.length - 1;		// only 1 wrong answer allowed
 		
 	$scope.computeResult = function () {
 		console.log($scope.answers);
@@ -38,7 +164,11 @@ app.controller('MainCtrl', function($scope, $state, $ionicPopup) {
 		}
 		
 		console.log('Num of correct answers = ' + $scope.numCorrectAns);
-		//$scope.answers = [null, null, null]; 	// reset form
+		
+		if ($scope.numCorrectAns >= $scope.minCorrectAns) {
+			resetAnswers(); 			// reset form
+		}
+		
 		$state.go('result');
     };
 
